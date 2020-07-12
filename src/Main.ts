35a7a6f7dd9e4582ba2e99688aaba8ec11b175eb
McBen <viertelvor12@gmx.net>
2020-07-12 19:46:02 +0200

    Tut - 2

--- src/Main.ts
+++ src/Main.ts
@@ -1,16 +1,40 @@
 import * as Plugin from "iitcpluginkit";
 
-
 class CountPortals implements Plugin.Class {
 
-    init() {
+    init(): void {
         console.log("CountPortals " + VERSION);
 
         require("./styles.css");
 
-        alert("Hello World");
+        this.createButtons();
     }
 
+
+    private createButtons(): void {
+        $("#toolbox").append(
+            $("<a>", {
+                text: "Count",
+                click: () => this.doCount()
+            })
+        )
+
+
+        const toolbarGroup = $("<div>", { class: "leaflet-bar leaflet-control" })
+            .append(
+                $("<a>", {
+                    class: "mybutton leaflet-bar-part",
+                    click: () => this.doCount()
+                })
+            );
+
+        const parent = $(".leaflet-top.leaflet-left", window.map.getContainer());
+        parent.append(toolbarGroup);
+    }
+
+    doCount(): void {
+        alert("Hello World");
+    }
 }
 
 
